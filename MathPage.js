deebb8fcb0545b08656bbe87977d58ff08e5c543
Tatiana <tatiana.lyubavskaya@gmail.com>
2024-11-06 12:47:05 +0200

    Math result checker added

--- MathPage.js
+++ MathPage.js
@@ -8,19 +8,18 @@ import {
 } from "react-native";
 import { Icon, ListItem, Button } from "@rneui/themed";
 import { useState, useEffect } from "react";
-import {mathTasksList} from "./MathTasksList";
+import { mathTasksList } from "./MathTasksList";
 import generateTasks from "./TasksGenerator";
 import EmptyList from "./EmptyMathTasksList";
-
 
 export default function MathPage({ navigation }) {
   const [expanded, setExpanded] = useState(false);
   const [taskSelection, setTaskSelection] = useState({
-    name: "Please select a task"
+    name: "Please select a task",
   });
-   const [resultsForm, setResultsForm] = useState([]);
+  const [resultsForm, setResultsForm] = useState([]);
 
-   const defineEmptyMathListStyle = () => (
+  const defineEmptyMathListStyle = () => (
     <EmptyList isEmpty={taskSelection.taskNumber == null} />
   );
 
@@ -33,13 +32,10 @@ export default function MathPage({ navigation }) {
   };
 
   const handleGenerate = () => {
-
     const newExamples = generateTasks(taskSelection); // Generate tasks based on selected task
     setResultsForm(newExamples); // Set resultsForm to the generated tasks
     console.log("Generated examples:", newExamples);
-};
-  
-
+  };
 
   const hendleResultsForm = (text, index) => {
     setResultsForm((prevResultsForm) => {
@@ -48,8 +44,23 @@ export default function MathPage({ navigation }) {
 
       const allFilled = newExamplesSet.every((item) => item.userResult !== "");
 
-      // If all filled, update the state
+      // If all filled
       if (allFilled) {
+        newExamplesSet.forEach((item) => {
+          // Show or hide specific icon and correct result if the userResult is not or equal to sysResult
+          if (item.userResult != item.sysResult) {
+            item.iconSlashVisible = true;
+            item.iconOkVisible = false;
+            item.showSysResult = true;
+            // Set the Slash icon and correct result visible and hide Ok icon
+          } else {
+            item.iconSlashVisible = false;
+            item.iconOkVisible = true;
+            item.showSysResult = false;
+            // Hide Slash icon and set Ok icon visible
+          }
+        });
+
         // Set the updated state only if all are filled
         console.log("All inputs filled:", newExamplesSet);
       }
@@ -65,15 +76,57 @@ export default function MathPage({ navigation }) {
       {item.thirdNum && <Text style={styles.examples}>{item.thirdNum}</Text>}
       {item.fourthNum && <Text style={styles.examples}>{item.fourthNum}</Text>}
       <Text style={styles.examples}>=</Text>
-      <Text style={styles.examples}>{item.sysResult}</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Your result"
-        value={item.userResult}
-        onChangeText={(text) => hendleResultsForm(text, index)}
-        keyboardType="numeric"
-      />
+      <View style={{ position: "relative" }}>
+        <TextInput
+          style={styles.examples}
+          placeholder="U result"
+          value={item.userResult}
+          onChangeText={(text) => hendleResultsForm(text, index)}
+          keyboardType="numeric"
+        />
+
+        {item.iconSlashVisible && (
+          <Icon
+            name="slash"
+            type="font-awesome-5"
+            size={25}
+            color="#d97915"
+            containerStyle={{
+              position: "absolute",
+              left: 4,
+              top: "50%",
+              transform: [{ translateY: -11 }],
+            }}
+          />
+        )}
+        {item.showSysResult && (
+          <Text
+            style={{
+              position: "absolute",
+              left: 55,
+              fontSize: 24,
+              padding: 8,
+              fontWeight: "800",
+            }}
+          >
+            {item.sysResult}
+          </Text>
+        )}
+
+        {item.iconOkVisible && (
+          <Icon
+            name="like"
+            type="evilicon"
+            size={45}
+            color="#22903e"
+            containerStyle={{
+              position: "absolute",
+              left: 55,
+            }}
+          />
+        )}
+      </View>
     </View>
   );
 
@@ -113,9 +166,7 @@ export default function MathPage({ navigation }) {
               <ListItem.Chevron />
             </ListItem>
           ))}
-         
         </ScrollView>
-
       </ListItem.Accordion>
 
       <Button
@@ -123,24 +174,22 @@ export default function MathPage({ navigation }) {
         buttonStyle={styles.buttonStyle}
         titleStyle={{ color: "#0b67a2" }}
         icon={{
-            name:"start",
-            type:"material",
-            size:30,
-            color:"#d97915",
-            containerStyle: { marginRight: 10 },
+          name: "start",
+          type: "material",
+          size: 30,
+          color: "#d97915",
+          containerStyle: { marginRight: 10 },
         }}
         raised
         containerStyle={{
           width: 100,
-          height:45,
+          height: 45,
           marginVertical: 10,
           marginLeft: 10,
-         
         }}
-        
         onPress={handleGenerate}
       >
-       Start
+        Start
       </Button>
 
       <FlatList
@@ -151,7 +200,6 @@ export default function MathPage({ navigation }) {
         removeClippedSubviews={false}
         ListEmptyComponent={defineEmptyMathListStyle}
       />
-     
     </View>
   );
 }
@@ -177,8 +225,7 @@ const styles = StyleSheet.create({
     width: "auto",
     fontSize: 20,
   },
-  buttonStyle:{
-  flexDirection: 'row',
-  
-  },  
+  buttonStyle: {
+    flexDirection: "row",
+  },
 });
